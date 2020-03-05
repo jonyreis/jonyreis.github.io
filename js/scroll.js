616b43987f869096b6d97d86ab9b5e46212713b0
Jony Reis <jonyreiscardoso@gmail.com>
2020-03-04 23:09:44 -0300

    refactoring scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,19 @@
-const go = (e) => {
-    window.scroll({
-        top: document.querySelector(e)
-        .offsetTop,
-        left: 0,
-        behavior: 'smooth'
-     });
-  }
+function initScroll() {
+    const linkScroll = document.querySelectorAll('.js-menu a[href^="#"]')
+
+    function scrollToSection(event) {
+        event.preventDefault()
+        const item = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(item)
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
+
+    linkScroll.forEach(item => {
+        item.addEventListener('click', scrollToSection)
+    })
+}
+
+initScroll()
